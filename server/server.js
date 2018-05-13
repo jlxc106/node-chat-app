db3f782cb24f01904983c2ab52de15a8303f642f
jlxc106 <jaylim106@gmail.com>
2018-05-12 18:21:47 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 3000; 
+
+var app = express();
+
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+})
